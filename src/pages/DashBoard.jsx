d667134fbac23995f42e94f1811c395ef33d5b4f
Антон <vash_email@email.com>
2025-08-27 20:06:47 +0500

    fix the skeleton issue again

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -10,7 +10,18 @@ function DashBoard() {
   if (isLoading) {
     return (
       <section className="mt-25 mb-20 grid grid-cols-4 gap-5 mx-auto max-w-[700px]">
-        <Skeleton count={10} height={50}/>
+        <Skeleton className="w-full h-[150px]" />
+        <Skeleton className="w-full h-[150px]" />
+        <Skeleton className="w-full h-[150px]" />
+        <Skeleton className="w-full h-[150px]" />
+        <Skeleton className="w-full h-[150px]" />
+        <Skeleton className="w-full h-[150px]" />
+        <Skeleton className="w-full h-[150px]" />
+        <Skeleton className="w-full h-[150px]" />
+        <Skeleton className="w-full h-[150px]" />
+        <Skeleton className="w-full h-[150px]" />
+        <Skeleton className="w-full h-[150px]" />
+        <Skeleton className="w-full h-[150px]" />
       </section>
     );
   }
